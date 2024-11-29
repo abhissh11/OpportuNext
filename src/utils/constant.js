bd9f0bde720302a2c38c8de4a50ef0ab6c12a7cb
Abhishek Kumar <dev.abhishekkr@gmail.com>
2024-11-30 00:32:10 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,3 @@
 // export const serverURL = "http://localhost:5000";
-// export const serverURL = "https://oppurtunext-backend-1.onrender.com";
-export const serverURL = "https://oppurtunext-backend-production.up.railway.app";
+export const serverURL = "https://oppurtunext-backend-1.onrender.com";
 // export const serverURL = "https://oppurtunext-backend-production.up.railway.app"
